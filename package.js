8800bef8ea08010b1164c36cc2b278d1bba6e822
Taras Dolynyuk <trsdln@gmail.com>
2016-09-20 14:14:27 +0300

    Fix url to repo

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'trsdln:modals',
-  version: '0.6.0',
+  version: '0.6.1',
   // Brief, one-line summary of the package.
   summary: 'Provides easy way to control your modals',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/trsdln/modals',
+  git: 'https://github.com/trsdln/meteor-modals',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
